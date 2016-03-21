899f479f9b31a3c87df17b7d76be5cc6eeb36439
baiyidjp <tztddong@dongjiangpengdemac-mini.local>
2016-03-21 14:54:28 +0800

    Initial Commit

---
+++ CustomScreenshot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomScreenshot
+//
+//  Created by tztddong on 16/3/21.
+//  Copyright © 2016年 dongjiangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
